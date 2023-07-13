6c08a020e47a2cfd6ff6484453af0b8454e79add
Simphiwe Mbatha <snsmbatha@gmail.com>
2023-07-13 13:22:16 -0700

    sized

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,2 @@
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
-*/
-
-int main(void)
-{
-
-printf("size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long in));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
-return (0);
-}
-
+-m32 -o size32 2> /tmp/32
+-m64 -o size64 2> /tmp/64
