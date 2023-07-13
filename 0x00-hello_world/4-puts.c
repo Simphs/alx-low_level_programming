47ded99ddb0129fd9b52d8e198278555bc5cb2a7
Simphiwe Mbatha <snsmbatha@gmail.com>
2023-07-13 12:17:20 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - A program that will output a line 
  *
- * return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
