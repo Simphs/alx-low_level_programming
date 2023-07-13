f465256175b876bcfc07235d524314e47e6fefbe
Simphiwe Mbatha <snsmbatha@gmail.com>
2023-07-13 12:38:08 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - script using printf
  *
- * Return: 0
+ * Return: Always  0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
