95f0d3b41faabcc00b6cd869813b5c2c3db68641
Simphiwe Mbatha <snsmbatha@gmail.com>
2023-07-15 06:46:31 -0700

    numbe

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main(){
+    int singleDigBaseTen;
+    for(singleDigBaseTen=0; singleDigBaseTen<10; singleDigBaseTen++){
+        printf("%d", singleDigBaseTen);
+    }
+    printf("\n");
+
+    return(0);
+}
