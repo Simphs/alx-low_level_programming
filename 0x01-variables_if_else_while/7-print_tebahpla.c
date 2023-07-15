23b9c52475bd751211a150f4c255deacce72ea8a
Simphiwe Mbatha <snsmbatha@gmail.com>
2023-07-15 12:18:53 -0700

    tebhla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+main - entry point
+return: Always 0(Success)
+*/
+
+int main(void)
+{
+
+         int c;
+
+	 c = 'z';
+         while
+	          (c<= 'a') {
+		        putchar(c);
+			c--;
+		   }
+	putchar('\n');
+	return(0);
+}
