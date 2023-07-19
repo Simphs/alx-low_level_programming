7bc23ba787c70036d597d7c83736635c3f1ad917
Simphiwe Mbatha <snsmbatha@gmail.com>
2023-07-19 12:14:10 -0700

    alphy

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
 /**
-main - entry point
-
-Description: prints alphabets 10 times
-
-return: Always 0(Success)
-*/
+ * print_alphabet_x10 - prints alphabets 10 times
+ *
+ * Return: 0 Always
+ */
 void print_alphabet_x10(void)
 {
+	int i;
+	int j;
 
-         int i;
-	 int j;
-	 
-	 for (j= 1; i <=10; i++)
-	 {
-                 for (j = 97; j <=122; j++)
-         	 {
-                        _putchar(j);
-                 }
-                 _putchar('\n');
-         }
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }
