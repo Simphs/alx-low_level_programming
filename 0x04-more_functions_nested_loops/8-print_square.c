1ca255dc421163bc122671bb7527145bc0f8a900
Simphiwe Mbatha <snsmbatha@gmail.com>
2023-07-20 16:32:59 -0700

    commi

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,14 +6,14 @@
  */
 void print_square(int size)
 {
-	if(size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	} else
 	{
 		int i, j;
 
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
