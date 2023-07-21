f85103f27e975514bad5a78e2ff3d571d7999712
Simphiwe Mbatha <snsmbatha@gmail.com>
2023-07-21 15:04:59 -0700

    multi

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,11 +4,10 @@
  * mul - multiply 2 int
  * @a: first int
  * @b: second int
- * Return: Always 0
+ * Return: Results
  */
 
 int mul(int a, int b)
 {
-	int result = a * b;
-	return (result);
+	return (a * b);
 }
