acb619b870e380ddf1ae9c4f5295310314c1b9d7
Simphiwe Mbatha <snsmbatha@gmail.com>
2023-07-21 18:01:35 -0700

    diagon

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_diagonal line on the terminal
- * @n: number of times the character \ should be printed
+ * print_diagonal - line on the terminal
  *
+ * @n: number of times the character \ should be printed
  */
 
 void print_diagonal(int n)
