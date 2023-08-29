3ef50dccfe8840b1e3211d35ffea7f57a9e7b3b5
Simphs <snsmbatha@gmail.com>
2023-08-29 13:33:30 -0700

    len

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,5 @@
-#include "lists.h"
 #include <stdio.h>
-#include "header_file.h"
+#include "stddef.h"
 
 size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
